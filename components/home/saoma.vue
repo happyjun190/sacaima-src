715c34a7145f09e0ad296aac1414bca9e7fe0087
shenjun-home <shenjun-home@126.com>
2016-08-16 20:25:24 +0800

    saoma modify

--- components/home/saoma.vue
+++ components/home/saoma.vue
@@ -12,6 +12,10 @@
   import encryption from 'src/assets/js/encryption.js'
 
   var scan = null;
+  //扩展API加载完毕后调用onPlusReady回调函数
+  document.addEventListener( "plusready", onPlusReady, false );
+
+
   var dataItem;
   export default {
 		name: "checkPublished",
@@ -42,40 +46,32 @@
 			}
 		},
 		methods: {
-      // 扩展API加载完毕后调用onPlusReady回调函数
-      //document.addEventListener( "plusready", onPlusReady, false );
-      // 扩展API加载完毕，现在可以正常调用扩展API
-      onPlusReady() {
-        var e = document.getElementById("scan");
-        e.removeAttribute( "disabled" );
-      },
-
       onmarked( type, result ) {
         var text = '未知: ';
         switch(type){
           case plus.barcode.QR:
-          text = 'QR: ';
-          break;
+            text = 'QR: ';
+            break;
           case plus.barcode.EAN13:
-          text = 'EAN13: ';
-          break;
+            text = 'EAN13: ';
+            break;
           case plus.barcode.EAN8:
-          text = 'EAN8: ';
-          break;
+            text = 'EAN8: ';
+            break;
         }
 
       },
-      startRecognize() {				//调用摄像头
+      startRecognize() {//调用摄像头
         scan = new plus.barcode.Barcode('bcid');
         scan.onmarked = onmarked;
       },
-      startScan() {					//开始扫描
+      startScan() {//开始扫描
         scan.start();
       },
-      cancelScan() {					//取消扫描
+      cancelScan() {//取消扫描
         scan.cancel();
       },
-      setFlash() {					//开启闪光灯
+      setFlash() {//开启闪光灯
         scan.setFlash();
       }
 		}
